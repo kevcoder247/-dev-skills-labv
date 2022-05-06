46939e4fc81ba6b1effc1fc0645391dc437b374f
)- <a-@-s-MacBook-Pro.local>
2022-05-06 08:21:15 -0400

    button click function added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-$(console.log('hello'))
+//Makes sure no skills appear when page is first loaded
+//when something is entered in the input prepend it to the when button is clicked
+//remove a skill when clicked
+
+
+
+//when button clicked console.log the text from the input field
+$('.button').click(function(){
+    const input = $('.input').val();
+    $('.cointainer').html(`<p>${input}</p>`)
+    console.log(input)
+})
