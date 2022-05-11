944f52dff62c971aea8bcec525145ba0726a084d
)- <a-@-s-MacBook-Pro.local>
2022-05-11 05:57:15 -0400

    js added when button clicked inpu, the text from the input field is added to the skill list

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-//Makes sure no skills appear when page is first loaded
-//when something is entered in the input prepend it to the when button is clicked
-//remove a skill when clicked
+//Change the text of the h1 to the given string in new H1
+// const newH1 = 'This is the new H1';
+// $('.heading').text(newH1);
+
+//change the text of h1 to a p tag
+// $('.heading').html('<p>this is now a paragraph</p>')
+
+//changes p to a new paragraph
+// $('.newSkill').html('<p>CODER</p')
 
 
-
-//when button clicked console.log the text from the input field
+//when button clicked log what was typed in the input and change P element
 $('.button').click(function(){
-    const input = $('.input').val();
-    $('.cointainer').html(`<p>${input}</p>`)
+    //take what is typed in the input feild and save it to the input variable
+    let input = $('.input').val();
     console.log(input)
-})
+    //create a variable new skill and append it to the ul when button clicked
+    const $newSkill = $(`<li>${input}</li>`);
+    $('.skillList').append($newSkill);
+    input = '';
+});
